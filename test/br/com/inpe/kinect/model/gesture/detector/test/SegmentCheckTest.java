630c3844283ef8b1d19d1b269809c6bcaefa72e0
Heitor Carneiro <heitorguerrac@gmail.com>
2015-06-25 15:58:05 -0300

    structure test

---
+++ test/br/com/inpe/kinect/model/gesture/detector/test/SegmentCheckTest.java
@@ -0,0 +1,64 @@
+package br.com.inpe.kinect.model.gesture.detector.test;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SegmentCheckTest {
+	/**
+	 * Instance Variable
+	 */
+
+	/**
+	 * Initialize Variable
+	 */
+	@Before
+	public void build() {
+
+	}
+
+	@Test
+	public void Test1() {
+		Object expected = null;
+		Object actual = null;
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void Test2() {
+		Object expected = null;
+		Object actual = null;
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void Test3() {
+		Object expected = null;
+		Object actual = null;
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void Test4() {
+		Object expected = null;
+		Object actual = null;
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void Test5() {
+		Object expected = null;
+		Object actual = null;
+		Assert.assertEquals(expected, actual);
+	}
+
+	/**
+	 * Clear Variable
+	 */
+	@After
+	public void end() {
+		// set variable to null
+		System.gc();
+	}
+}
