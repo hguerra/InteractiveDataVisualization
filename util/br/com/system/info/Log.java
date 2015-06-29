6e93e7dc89fe1c0b5cb34765613d77dbcc002dd2
Heitor Carneiro <heitorguerrac@gmail.com>
2015-06-29 13:47:16 -0300

    project diagram

---
+++ util/br/com/system/info/Log.java
@@ -0,0 +1,12 @@
+package br.com.system.info;
+
+import java.io.Serializable;
+
+public class Log implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public Log() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
