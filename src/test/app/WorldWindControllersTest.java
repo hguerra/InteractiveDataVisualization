9bf8d4f6524db2da488cbdc856e94bd98a52ee57
Heitor Carneiro <heitorguerrac@gmail.com>
2016-01-26 10:01:49 -0200

    add CommentLayer controller

--- src/test/app/WorldWindControllersTest.java
+++ src/test/app/WorldWindControllersTest.java
@@ -1,5 +1,6 @@
 package test.app;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Insets;
 
@@ -9,10 +10,12 @@ import br.inpe.message.properties.DefaultColors;
 import br.inpe.util.FilePathTest;
 import br.inpe.worldwind.controller.LayerController;
 import br.inpe.worldwind.controller.ShapefileController;
+import br.inpe.worldwind.defaultcontroller.CommentLayer;
 import br.inpe.worldwind.defaultcontroller.ScreenAnnotationLayer;
 import br.inpe.worldwind.defaultcontroller.ShapefileLayer;
 import gov.nasa.worldwind.BasicModel;
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.geom.Position;
 
 public class WorldWindControllersTest extends JFrame {
 	/**
@@ -26,6 +29,7 @@ public class WorldWindControllersTest extends JFrame {
 	private LayerController screenAnnotation;
 	private LayerController polygon2d;
 	private ShapefileController shpController;
+	private LayerController comment;
 
 	public WorldWindControllersTest() {
 		worldWindConfig();
@@ -39,17 +43,22 @@ public class WorldWindControllersTest extends JFrame {
 		screenAnnotation.asyncDraw();
 
 		// Polygon2D
-//		 JDBCDao<vegtype_2000> dao = new JDBCDao<>();
-//		 List<vegtype_2000> vegtype2000 = dao.getAll(vegtype_2000.class);
-//		 List<GeometryRecord> geometryRecords = new ArrayList<>();
-//		 geometryRecords.addAll(vegtype2000);
-//		 polygon2d = new Polygon2DLayer(wwd, geometryRecords);
-//		 polygon2d.asyncDraw();
+		// JDBCDao<vegtype_2000> dao = new JDBCDao<>();
+		// List<vegtype_2000> vegtype2000 = dao.getAll(vegtype_2000.class);
+		// List<GeometryRecord> geometryRecords = new ArrayList<>();
+		// geometryRecords.addAll(vegtype2000);
+		// polygon2d = new Polygon2DLayer(wwd, geometryRecords);
+		// polygon2d.asyncDraw();
 
 		// ShapefileController
 		shpController = new ShapefileLayer(wwd, "attr");
 		shpController.addShapefile(FilePathTest.VEGTYPE_2000, DefaultColors.getOriginalColors1());
 		shpController.asyncDraw();
+
+		// CommentLayer
+		comment = new CommentLayer(wwd, new Position(Position.fromDegrees(-23, -45), 0.1), new Dimension(480, 145),
+				"\n\n\n\n\n Interactive Data Visualization", "images/ccst-novo2.png", 0.2, Color.white, Color.white);
+		comment.asyncDraw();
 	}
 
 	private final void worldWindConfig() {
