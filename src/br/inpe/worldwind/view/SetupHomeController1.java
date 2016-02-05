156710c691a898da8a6dfc89e2cb10a1b58fdf1f
Heitor Carneiro <heitorguerrac@gmail.com>
2016-02-05 16:32:17 -0200

    add event change setup panel

--- src/br/inpe/worldwind/view/SetupHomeController1.java
+++ src/br/inpe/worldwind/view/SetupHomeController1.java
@@ -3,8 +3,17 @@ package br.inpe.worldwind.view;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.JOptionPane;
+
+import br.inpe.worldwind.view.resources.Resource;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ContextMenu;
@@ -15,6 +24,7 @@ import javafx.scene.control.ToggleButton;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import test.app.WorldWindControllersTest;
 
 public class SetupHomeController1 implements Initializable {
 	@FXML
@@ -83,10 +93,101 @@ public class SetupHomeController1 implements Initializable {
 	@FXML
 	private ImageView imgMock;
 
+	/* Backup */
+	private ObservableList<Node> elementsSetupPanelBasic;
+	private ObservableList<Node> elementsSetupPanelLayer;
+	private ObservableList<Node> elementsSetupPanelDatabase;
+	private ObservableList<Node> elementsSetupPanelKinect;
+	private ObservableList<Node> elementsSetupPanelProfile;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+		/* Load all panes before */
+		initSetupPane();
+
+		/* add events */
+
+		btnStart.setOnAction(event -> new WorldWindControllersTest().run());
+		btnClose.setOnAction(event -> {
+
+			clearPaneSetup();
+
+			Task<Void> task = new Task<Void>() {
+				@Override
+				protected Void call() throws Exception {
+					Thread.sleep(2000);
+					return null;
+				}
+
+			};
+			task.setOnSucceeded(e -> {
+				JOptionPane.showMessageDialog(null, "Bye Bye!");
+			});
+			new Thread(task).start();
+		});
+
+		btnGlobe.setOnAction(event -> this.paneSetup.getChildren().addAll(elementsSetupPanelBasic));
+
+		btnLayer.setOnAction(event -> this.paneSetup.getChildren().addAll(elementsSetupPanelLayer));
+
+		btnDataBase.setOnAction(event -> this.paneSetup.getChildren().addAll(elementsSetupPanelDatabase));
+
+		btnKinect.setOnAction(event -> this.paneSetup.getChildren().addAll(elementsSetupPanelKinect));
+
+		btnProfile.setOnAction(event -> this.paneSetup.getChildren().addAll(elementsSetupPanelProfile));
 
 	}
 
+	public boolean addElementsPaneSetup(Node... elements) {
+		return getPaneSetupChildren().addAll(elements);
+	}
+
+	public void clearPaneSetup() {
+		getPaneSetupChildren().clear();
+	}
+
+	private ObservableList<Node> getPaneSetupChildren() {
+		return this.paneSetup.getChildren();
+	}
+
+	/* Load Setup panel */
+	protected boolean loadSetupPane(URL location) {
+		try {
+			Parent parent = FXMLLoader.load(location);
+			return this.paneSetup.getChildren().addAll(parent.getChildrenUnmodifiable());
+		} catch (Exception e) {
+			System.err.println(e);
+			return false;
+		}
+	}
+
+	protected boolean loadSetupPane(ObservableList<Node> elements, URL location) {
+		try {
+			Parent parent = FXMLLoader.load(location);
+			ObservableList<Node> parentList = parent.getChildrenUnmodifiable();
+			elements.addAll(parentList);
+			return true;
+		} catch (Exception e) {
+			System.err.println(e);
+			return false;
+		}
+	}
+
+	private void initSetupPane() {
+		this.elementsSetupPanelBasic = FXCollections.observableArrayList();
+		this.elementsSetupPanelLayer = FXCollections.observableArrayList();
+		this.elementsSetupPanelDatabase = FXCollections.observableArrayList();
+		this.elementsSetupPanelKinect = FXCollections.observableArrayList();
+		this.elementsSetupPanelProfile = FXCollections.observableArrayList();
+		// loadSetupPane(elementsSetupPanelBasic,
+		// Resource.getPaneSetupBasicFXML());
+		loadSetupPane(elementsSetupPanelLayer, Resource.getPaneSetupLayerFXML());
+		// loadSetupPane(elementsSetupPanelDatabase,
+		// Resource.getPaneSetupDatabaseFXML());
+		// loadSetupPane(elementsSetupPanelKinect,
+		// Resource.getPaneSetupKinectFXML());
+		// loadSetupPane(elementsSetupPanelProfile,
+		// Resource.getPaneSetupProfileFXML());
+
+	}
 }
--- src/br/inpe/worldwind/view/resources/Resource.java
+++ src/br/inpe/worldwind/view/resources/Resource.java
@@ -7,23 +7,40 @@ public class Resource {
 	 * Parent parent = FXMLLoader.load(getClass().getResource(RESOURCE_PATH));
 	 */
 	public static final String RESOURCE_PATH = "/br/inpe/worldwind/view/resources/";
-	
-	public static URL getURL(String filename){
+
+	public static URL getURL(String filename) {
 		return Resource.class.getResource(filename);
 	}
-	
-	public static URL getLoginFXML(){
+
+	public static URL getLoginFXML() {
 		return getURL("login-fxml.fxml");
 	}
-	
-	public static URL getHomeFXML(){
-		return getURL("home-fxml.fxml");
-	}
-	public static URL getHomeDefaultFXML(){
+
+	public static URL getHomeDefaultFXML() {
 		return getURL("home-default-fxml.fxml");
 	}
-	
-	public static String getStylesheet(String stylesheet){
+
+	public static URL getPaneSetupBasicFXML() {
+		return getURL("pane-setup-basic-fxml.fxml");
+	}
+
+	public static URL getPaneSetupLayerFXML() {
+		return getURL("pane-setup-layer-fxml.fxml");
+	}
+
+	public static URL getPaneSetupDatabaseFXML() {
+		return getURL("pane-setup-database-fxml.fxml");
+	}
+
+	public static URL getPaneSetupKinectFXML() {
+		return getURL("pane-setup-kinect-fxml.fxml");
+	}
+
+	public static URL getPaneSetupProfileFXML() {
+		return getURL("pane-setup-profile-fxml.fxml");
+	}
+
+	public static String getStylesheet(String stylesheet) {
 		return getURL(stylesheet).toExternalForm();
 	}
 }
