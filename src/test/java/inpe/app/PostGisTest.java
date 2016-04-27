a326af7351a3ace7750dd198edaff87b4c1fdabe
Heitor Carneiro <heitorguerrac@gmail.com>
2016-04-27 17:13:00 -0300

    refactor: convert to maven project

---
+++ src/test/java/inpe/app/PostGisTest.java
@@ -0,0 +1,97 @@
+package inpe.app;
+
+import org.postgis.Geometry;
+import org.postgis.PGgeometry;
+import org.postgis.Polygon;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * 
+ * @author Heitor
+ *
+ *
+ *         --SELECT ST_AsText(geom) FROM vegtype_2000; -- return
+ *         "MULTIPOLYGON(((-61 5,-60.5 5,-60.5 4.5,-61 4.5,-61 5)))"
+ * 
+ *         --SELECT st_x(st_centroid(geom)),st_y(st_centroid(geom)),
+ *         ST_AsText(geom) FROM vegtype_2000; -- return -60.75, 4.75,
+ *         "MULTIPOLYGON(((-61 5,-60.5 5,-60.5 4.5,-61 4.5,-61 5)))"
+ * 
+ *         --SELECT ST_AsText( (ST_Dump(geom)).geom ) FROM vegtype_2000;
+ *         --return "POLYGON((-61 5,-60.5 5,-60.5 4.5,-61 4.5,-61 5))"
+ *
+ *
+ *
+ *
+ */
+public class PostGisTest {
+	public static void main(String[] args) {
+		java.sql.Connection conn;
+		try {
+			/*
+			 * Load the JDBC driver and establish a connection.
+			 */
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://localhost:5432/vegetation_scenario";
+			conn = DriverManager.getConnection(url, "postgres", "11");
+			/*
+			 * Add the geometry types to the connection. Deve converter a
+			 * conex�o com a conex�o espec�fica pgsql implementation before
+			 * calling the addDataType() method.
+			 */
+			((org.postgresql.PGConnection) conn).addDataType("geometry", Class.forName("org.postgis.PGgeometry"));
+			((org.postgresql.PGConnection) conn).addDataType("box3d", Class.forName("org.postgis.PGbox3d"));
+			/*
+			 * Create a statement and execute a select query.
+			 */
+			Statement s = conn.createStatement();
+
+			ResultSet r = s.executeQuery("SELECT (ST_Dump(geom)).geom, attr FROM vegtype_2000");
+			while (r.next()) {
+				/*
+				 * Retrieve the geometry as an object then cast it to the
+				 * geometry type. Print things out.
+				 */
+
+				/**
+				 * select geom,attr from vegtype_2000
+				 */
+				// PGgeometry geom = (PGgeometry) r.getObject(1);
+
+				// int attr = r.getInt(2);
+
+				// System.out.println("Attr " + attr + ":");
+				// System.out.println(geom.toString());
+				// System.out.println(geom.getGeometry().getType());
+				
+				/**
+				 * SELECT ST_AsText((ST_Dump(geom)).geom) FROM vegtype_2000
+				 */
+				// String area = r.getString("ST_AsText");
+				// System.out.println(area);
+
+				/**
+				 * SELECT (ST_Dump(geom)).geom, attr FROM vegtype_2000
+				 */
+				PGgeometry geom = (PGgeometry)r.getObject("geom");
+				int attr = r.getInt("attr");
+			
+				if (geom.getGeoType() == Geometry.POLYGON) {
+					Polygon polygon = (Polygon) geom.getGeometry();
+					for (int i = 0; i < polygon.numPoints(); i++) {
+						System.out.println("-->" + polygon.getPoint(i).y + ", " + polygon.getPoint(i).x);
+					}
+				}
+
+
+			}
+			s.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/test/java/inpe/worldwind/engine/ShapefilePropertiesTest.java
@@ -0,0 +1,86 @@
+package inpe.worldwind.engine;
+
+import br.inpe.triangle.defaultproperties.DefaultColors;
+import br.inpe.triangle.defaultproperties.DefaultFilePath;
+import br.inpe.worldwind.controller.ShapefileController;
+import br.inpe.worldwind.engine.ShapefileProperties;
+import gov.nasa.worldwind.formats.shapefile.Shapefile;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+import java.util.Map;
+import java.util.Set;
+
+public class ShapefilePropertiesTest {
+	private ShapefileProperties prop;
+	private Shapefile shp;
+	private String attributeName;
+
+	@Before
+	public void init() throws Exception {
+		prop = new ShapefileProperties();
+		shp = ShapefileController.createShapefile(DefaultFilePath.VEGTYPE_2000);
+		attributeName = "attr";
+	}
+
+	@Test
+	public void testCreatePolygonColorsEmptyColor() {
+		// Empty
+		Color[] interiorMaterial = new Color[] {};
+
+		Map<Object, Color> colors = prop.createPolygonColors(shp, attributeName, interiorMaterial);
+		Object expected = 7;
+		Object actual = colors.size();
+
+		System.out.println("Empty:" + colors);
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testCreatePolygonColorsOneColor() {
+		// One color
+		Color[] interiorMaterial = new Color[] { Color.red };
+
+		Map<Object, Color> colors = prop.createPolygonColors(shp, attributeName, interiorMaterial);
+		Object expected = 7;
+		Object actual = colors.size();
+
+		System.out.println("One:" + colors);
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testCreatePolygonColors() {
+		// default color
+		Color[] interiorMaterial = DefaultColors.getOriginalColors1();
+
+		Map<Object, Color> colors = prop.createPolygonColors(shp, attributeName, interiorMaterial);
+		Object expected = 7;
+		Object actual = colors.size();
+
+		System.out.println("Default:" + colors);
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testGetDataColumns() {
+		Set<String> dataColumns = ShapefileProperties.getShapefileColumns(shp);
+		Object expected = 1;
+		Object actual = dataColumns.size();
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@After
+	public void after() {
+		prop = null;
+		shp = null;
+		attributeName = null;
+	}
+}
