49741bc648244f42d473854d66b093a1b309328e
Heitor Carneiro <heitorguerrac@gmail.com>
2016-05-11 14:34:36 -0300

    add enums in GeoFormat

--- src/main/java/br/inpe/worldwind/view/controllers/impl/SceneLayerController.java
+++ src/main/java/br/inpe/worldwind/view/controllers/impl/SceneLayerController.java
@@ -1,7 +1,9 @@
 package br.inpe.worldwind.view.controllers.impl;
 
+import br.inpe.gdal.transform.GeoFormat;
 import br.inpe.worldwind.view.controllers.ApplicationSceneController;
 import br.inpe.worldwind.view.controllers.SceneView;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
@@ -23,11 +25,12 @@ public class SceneLayerController extends ApplicationSceneController {
     private TextArea txtOgrInfo;
 
     @FXML
-    private ComboBox comboOutputFormat;
+    private ComboBox<GeoFormat> comboOutputFormat;
     
     @Override
     protected void initPaneSetup() {
-
+        this.comboOutputFormat.setItems(FXCollections.observableArrayList(GeoFormat.values()));
+        this.comboOutputFormat.getSelectionModel().selectFirst();
     }
 
     @Override
